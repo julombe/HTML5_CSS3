0ed8a5a6d5b794b61869b458747f80415d0d8b77
José Ulombe <jose.ulombe@hotmail.com>
2020-11-24 23:26:54 +0100

    update server

--- routes.js
+++ routes.js
@@ -2,6 +2,13 @@ const express = require("express")
 const routes = express.Router()
 
 
+//creating routes
+routes.get("/", function(req, res) {
+    return res.render("index")
+})
+
+
+
 module.exports = routes
 
 
